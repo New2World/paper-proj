36957ff7b0aa483cbabdbfe45a83b20e0d69a0a7
Ruiqi Wang <wangrq@udel.edu>
2019-07-26 16:49:48 -0400

    figures with larger font size

--- src/test.py
+++ src/test.py
@@ -10,40 +10,34 @@ from model import OurModel
 raw_data = np.load("../data/one-hot-encoding.npz")
 data = raw_data["encoding"]
 popularity = raw_data["popularity"]
-adj_matrix = data.dot(data.T)
-adj_matrix = adj_matrix / np.max(adj_matrix)
+n_threads = data.shape[0]
 
-gcn = OurModel(data.shape[1], 300, 300, np.zeros(adj_matrix.shape))
-
+gcn = OurModel(data.shape[1], 300, 300, np.eye(n_threads))
 gcn.load_state_dict(torch.load("../checkpoints/model.pt"))
-gcn.eval()
 learned_adj = gcn.gcn.gc1.adj_matrix.detach().numpy()
-output = gcn(torch.from_numpy(data).type(torch.FloatTensor)).detach().numpy()
 partial_learned = learned_adj[:100,:100]
-# print(partial_learned)
 
-# np.save("prediction.npy", output)
-# np.save("adjacent.npy", learned_adj)
-
-# output = np.load("output.npy")
-
+output = np.load("../res/prediction.npy")
 mae = np.mean(np.abs(popularity-output))
 print(f"MAE loss: {mae}")
-# plt.xlabel('post')
-# plt.ylabel('popularity')
-# x = np.arange(100)
-# plt.scatter(x, popularity[:100], c='r', marker='x', label='ground truth')
-# plt.scatter(x, output[:100], c='b', marker='o', label='prediction')
-# plt.legend()
 matplotlib.rcParams['font.family'] = 'Times New Roman'
-# sb.heatmap(adj_matrix[:100,:100], cmap="Blues", xticklabels=10, yticklabels=10)
-# plt.title("initial adjacent matrix")
-# plt.xlabel("post ID")
-# plt.ylabel("post ID")
-# plt.savefig("initial_adjacent_matrix.pdf", format='pdf')
+matplotlib.rcParams['font.size'] = 16
+plt.xlabel('thread ID')
+plt.ylabel('popularity')
+x = np.arange(100)
+plt.scatter(x, popularity[:100], c='r', marker='x', label='ground truth')
+plt.scatter(x, output[:100], c='b', marker='o', label='prediction')
+plt.legend()
+plt.savefig("../res/popularity_prediction_100.pdf", format='pdf')
+plt.clf()
+sb.heatmap(adj_matrix[:100,:100], cmap="Blues", xticklabels=10, yticklabels=10)
+plt.title("initial adjacent matrix")
+plt.xlabel("thread ID")
+plt.ylabel("thread ID")
+plt.savefig("../res/initial_adjacent_matrix.pdf", format='pdf')
+plt.clf()
 sb.heatmap(partial_learned, cmap="Blues", xticklabels=10, yticklabels=10)
 plt.title("learned adjacent matrix")
-plt.xlabel("post ID")
-plt.ylabel("post ID")
-plt.savefig("learned_adjacent_matrix.pdf", format='pdf')
-plt.show()
+plt.xlabel("thread ID")
+plt.ylabel("thread ID")
+plt.savefig("../res/learned_adjacent_matrix.pdf", format='pdf')
